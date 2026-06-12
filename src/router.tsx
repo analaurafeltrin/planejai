7808410f4ff5291195aea9303ef2b0f63a0dbb41
analaurafeltrina <alaura.feltrin@gmail.com>
2026-06-11 21:17:28 -0300

    fix: add basename to router for subdirectory deployment

--- src/router.tsx
+++ src/router.tsx
@@ -4,22 +4,27 @@ import { RootLayout } from './components/layout/RootLayout'
 import { SimulationFormPage } from './pages/SimulationFormPage'
 import { SimulationResultsPage } from './pages/SimulationResultsPage'
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      element: <RootLayout />,
+      children: [
+        {
+          path: '/',
+          element: <SimulationFormPage />,
+        },
+        {
+          path: '/resultado/:id',
+          element: <SimulationResultsPage />,
+        },
+        {
+          path: '/historico',
+          element: <h1>Histórico de Simulações</h1>,
+        },
+      ],
+    },
+  ],
   {
-    element: <RootLayout />,
-    children: [
-      {
-        path: '/',
-        element: <SimulationFormPage />,
-      },
-      {
-        path: '/resultado/:id',
-        element: <SimulationResultsPage />,
-      },
-      {
-        path: '/historico',
-        element: <h1>Histórico de Simulações</h1>,
-      },
-    ],
+    basename: '/planejai',
   },
-])
+)
